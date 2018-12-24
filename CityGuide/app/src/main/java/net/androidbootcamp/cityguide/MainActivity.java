a7ee99824381829cc78d920ee5ab70ddcf6bb3b5
Blaine Smith <ringospy@gmail.com>
2018-12-24 17:17:33 -0600

    Ch 5: Changing theme along

--- CityGuide/app/src/main/java/net/androidbootcamp/cityguide/MainActivity.java
+++ CityGuide/app/src/main/java/net/androidbootcamp/cityguide/MainActivity.java
@@ -2,12 +2,14 @@ package net.androidbootcamp.cityguide;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        String [] attraction = {"Art Institute of Chicago", "Magnificent Mile", "Willis Tower", "Navy Pier", "Water Tower"};
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, attraction));
     }
 }
