5fc4116e8f7dd67d794f378b4cab5a58c700052a
Blaine Smith <ringospy@gmail.com>
2018-12-24 16:55:42 -0600

    Ch 5: Add image icon and reformat MainActivity

--- CityGuide/app/src/main/java/net/androidbootcamp/cityguide/MainActivity.java
+++ CityGuide/app/src/main/java/net/androidbootcamp/cityguide/MainActivity.java
@@ -1,13 +1,13 @@
 package net.androidbootcamp.cityguide;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        
     }
 }
