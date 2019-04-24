c1f04f3a25d388aa489cf30b466bab4c22ec2a1f
Blaine <Ringospy@gmail.com>
2019-04-23 20:07:32 -0500

    Camp Geiger v2

--- CampGeiger/app/src/main/java/com/example/campgeiger/MainActivity.java
+++ CampGeiger/app/src/main/java/com/example/campgeiger/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.campgeiger;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    final Context text = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner activities = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.activities));
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        activities.setAdapter(adapter);
+
+
+        activities.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String Text = String.valueOf(parent.getSelectedItem());
+                Toast.makeText(getApplicationContext(), Text,Toast.LENGTH_LONG).show();
+
+                final AlertDialog dialog = new AlertDialog(text);
+                dialog.setContentView(R.layout.custom);
+
+                if (Text.contentEquals("Help")){
+                    dialog.setMessage("hello");
+                    dialog.show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
